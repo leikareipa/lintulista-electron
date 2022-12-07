16e7932fcf99bbc5b524d7461364c987f57493f4
leikareipa <drilledicecream@gmail.com>
2022-12-07 20:51:19 +0200

    electron: Add some hardening

--- distributable/electron/entry.js
+++ distributable/electron/entry.js
@@ -5,40 +5,90 @@
  * 
  */
 
-const {app, BrowserWindow} = require("electron");
+const appName = "Lintulista";
+const listKey = process.argv?.[2];
 
-app.whenReady().then(()=>{
+if (typeof listKey !== "string") {
+    console.error("No list key specified.");
+    process.exit();
+}
+else if (!/^[a-z0-9]{9}$/.test(listKey)) {
+    console.error("Malformed list key.");
+    process.exit();
+}
+
+const Electron = require("electron");
+Electron.app.enableSandbox();
+Electron.app.whenReady().then(()=>{
     try {
+        set_web_request_handler();
         start_server();
         start_client();
     }
     catch (error) {
-        console.error("Lintulista crashed!", error);
+        console.error(`${appName} crashed!`, error);
         process.exit();
     }
-});
+    
+    function set_web_request_handler()
+    {
+        Electron.session.defaultSession.webRequest.onBeforeRequest({urls: ["*://*/*"]}, (details, callback)=>{
+            const URL = require('url').URL;
+            const parsedTargetUrl = new URL(details.url);
 
-function start_server() {
-    const http = require("http");
-    const {LL_ProcessRequest} = require("../server/process-request.js");
-    http.createServer(LL_ProcessRequest).listen(8080, "127.0.0.1");
-}
+            switch (parsedTargetUrl.host + parsedTargetUrl.pathname) {
+                // The user interacting with the list by adding/removing/modifying observations.
+                case "localhost:8080/": {
+                    if (
+                        (parsedTargetUrl.search !== `?list=${listKey}`) ||
+                        !["GET", "PUT", "DELETE"].includes(details.method)
+                    ){
+                        break;
+                    }
 
-function start_client() {
-    const mainWindow = new BrowserWindow({
-        width: 1280,
-        height: 720,
-        show: false,
-    });
+                    return callback({});
+                }
+                // The user logging in (POST) or out (DELETE).
+                case "localhost:8080/login": {
+                    if (!["POST"].includes(details.method)) {
+                        break;
+                    }
 
-    if (!process.env.LL_DEV) {
-        mainWindow.setMenu(null);
+                    return callback({});
+                }
+                default: break;
+            }
+            
+            console.log(`${appName}: Ignoring ${details.method} request to ${parsedTargetUrl.toString()}`);
+        });
     }
 
-    mainWindow.loadFile("../client/index.html", {hash: process.argv[2]});
+    function start_server() {
+        const http = require("http");
+        const {LL_ProcessRequest} = require("../server/process-request.js");
 
-    mainWindow.on("ready-to-show", ()=>{
-        mainWindow.show();
-        mainWindow.focus();
-    });
-}
+        http.createServer(LL_ProcessRequest).listen(8080, "127.0.0.1"); 
+    }
+
+    function start_client() {
+        const mainWindow = new Electron.BrowserWindow({
+            width: 1280,
+            height: 720,
+            show: false,
+        });
+
+        if (!process.env.LL_DEV) {
+            mainWindow.setMenu(null);
+        }
+
+        mainWindow.loadFile("../client/index.html", {hash: listKey});
+        
+        // The app isn't expected to open any new windows.
+        mainWindow.webContents.setWindowOpenHandler(()=>({action: "deny"}));
+
+        mainWindow.on("ready-to-show", ()=>{
+            mainWindow.show();
+            mainWindow.focus();
+        });
+    }
+});
